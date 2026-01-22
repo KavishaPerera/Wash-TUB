a27340663417c9b88b8386f1ed761ca28995911b
Mosh Hamadani <kavishaperera386@gmail.com>
2026-01-22 14:33:00 +0530

    fix: commented

--- frontend/src/pages/CustomerDashboard.jsx
+++ frontend/src/pages/CustomerDashboard.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './CustomerDashboard.css';
 
 const CustomerDashboard = () => {
+    // Navigations 
     const navigate = useNavigate();
     const [userName] = useState('Amandi');
 
